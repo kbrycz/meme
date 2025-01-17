5b1fd251dd52b85bdc2bf0dc090e06ec4a40ee25
Karl Brycz <kbrycz@umich.edu>
2025-01-17 13:14:15 -0600

    Initial Commit

---
+++ meme/memeApp.swift
@@ -0,0 +1,17 @@
+//
+//  memeApp.swift
+//  meme
+//
+//  Created by Karl Brycz on 1/17/25.
+//
+
+import SwiftUI
+
+@main
+struct memeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
